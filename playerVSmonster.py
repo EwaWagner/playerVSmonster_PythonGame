81dc60254f866ed2d723fedf5a3a2a4ef21af9f2
EwaWagner <74456960+EwaWagner@users.noreply.github.com>
2022-07-20 13:19:25 +0100

    Create playerVSmonster.py

---
+++ playerVSmonster.py
@@ -0,0 +1,13 @@
+import time
+
+time.sleep(1)
+print("State what is your name \n")
+username = input()
+time.sleep(1)
+print("\n" + username + ", you will be now fighting monsters\n")
+time.sleep(1)
+print("\nlets see how long you will last.")
+
+#player's stats
+playerHP = 100
+playerATK = 20
